4035c176e7fe07573e58e90c731ae521c630974f
jenyak <jenyak@users.noreply.github.com>
2020-06-27 21:52:13 +0300

    FIx EntityFilter missing createAlias

--- src/Filter/EntityFilter.php
+++ src/Filter/EntityFilter.php
@@ -19,6 +19,16 @@ final class EntityFilter implements FilterInterface
 {
     use FilterTrait;
 
+    protected static $uniqueAliasId = 0;
+
+    /**
+     * Generates dynamic alias from a given name.
+     */
+    protected static function createAlias(string $name): string
+    {
+        return str_replace('.', '_', $name).'_'.++static::$uniqueAliasId;
+    }
+
     public static function new(string $propertyName, $label = null): self
     {
         return (new self())
